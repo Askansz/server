e6c132d5c9a4340757877c07e37855490e76c409
Askansz <122436444+Askansz@users.noreply.github.com>
2025-09-28 23:43:24 +0200

    Update script.bat

--- script.bat
+++ script.bat
@@ -1,12 +1,3 @@
 @echo off
-set text=Hello, World!
-
-for /l %%i in (1,1,13) do (
-    setlocal enabledelayedexpansion
-    set char=!text:~%%i,1!
-    <nul set /p=!char!
-    timeout /t 0 >nul
-    endlocal
-)
-echo.
+echo Hello, World!
 pause
